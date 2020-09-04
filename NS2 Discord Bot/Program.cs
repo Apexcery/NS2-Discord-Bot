2365f96c56b930469a1a1f5b9458416ebc2bb082
Stuart Gleeson <stuart.gleeson98@outlook.com>
2020-09-04 21:34:58 +0100

    Add project files.

---
+++ NS2 Discord Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NS2_Discord_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
