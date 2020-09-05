78f28cf4d31411d2544143ea2eeac1a8a7b170ae
Stuart Gleeson <stuart.gleeson98@outlook.com>
2020-09-05 23:00:49 +0100

    Added KDR command and method to get profile name.

--- NS2-Discord-Bot/Commands/Main.cs
+++ NS2-Discord-Bot/Commands/Main.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -8,8 +9,7 @@ namespace NS2_Discord_Bot.Commands
 {
     public class Main : ModuleBase<SocketCommandContext>
     {
-        [Command("hive")]
-        public async Task HiveLookup(string steamId)
+        private async Task<(string ProfileUrl, HtmlDocument Document)> GetProfilePage(string steamId)
         {
             var web = new HtmlWeb();
             var doc = await web.LoadFromWebAsync($"https://observatory.morrolan.ch/players?filter={steamId}&last_active_after=2010-01-01");
@@ -19,25 +19,55 @@ namespace NS2_Discord_Bot.Commands
             if (profileUrl == "N/A")
             {
                 await ReplyAsync("Could not find a profile with that Steam ID");
+                return (null, null);
+            }
+
+            var url = "https://observatory.morrolan.ch" + profileUrl;
+            doc = await web.LoadFromWebAsync(url);
+
+            return (url, doc);
+        }
+
+        private string GetProfileName(HtmlDocument doc)
+        {
+            var header = doc.DocumentNode.SelectSingleNode("//div[contains(@class, 'page-header')]");
+            var headerText = header.InnerText;
+
+            headerText = headerText
+                .Remove(headerText.IndexOf("Updated:", StringComparison.Ordinal))
+                .Replace("\n", string.Empty);
+
+            return headerText;
+        }
+
+        [Command("hive")]
+        public async Task HiveLookup(string steamId)
+        {
+            var (profileUrl, document) = await GetProfilePage(steamId);
+
+            if (document == null || string.IsNullOrEmpty(profileUrl))
+            {
+                await ReplyAsync("Could not find a profile with that Steam ID");
                 return;
             }
 
-            doc = await web.LoadFromWebAsync("https://observatory.morrolan.ch" + profileUrl);
+            var profileName = GetProfileName(document);
 
-            var hiveStats = GetHiveStats(doc);
+            var (hiveSkill, hiveLevelExp, hiveScore, hiveScoreMin) = GetHiveStats(document);
 
-            if (hiveStats.HiveSkill == null || hiveStats.HiveLevelExp == null)
+            if (hiveSkill == null || hiveLevelExp == null || hiveScore == null || hiveScoreMin == null)
             {
                 await ReplyAsync("Could not find the Hive details for that profile");
                 return;
             }
 
             var embed = new EmbedBuilder()
-                .WithTitle("Hive Stats For: ")
-                .AddField("Skill:", hiveStats.HiveSkill)
-                .AddField("Level / Exp:", hiveStats.HiveLevelExp)
-                .AddField("Score:", hiveStats.HiveScore)
-                .AddField("Score / Min:", hiveStats.HiveScoreMin)
+                .WithTitle(profileName)
+                .WithUrl(profileUrl)
+                .AddField("Skill:", hiveSkill)
+                .AddField("Level / Exp:", hiveLevelExp)
+                .AddField("Score:", hiveScore)
+                .AddField("Score / Min:", hiveScoreMin)
                 .WithColor(Color.Orange)
                 .Build();
 
@@ -141,6 +171,128 @@ namespace NS2_Discord_Bot.Commands
             #endregion
 
             return (HiveSkill: replacedSkillText, HiveLevelExp: replacedLevelExpText, HiveScore: replacedScoreText, HiveScoreMin: replacedScoreMinText);
+        }
+
+        [Command("kdr")]
+        public async Task KDRLookup(string steamId)
+        {
+            var (profileUrl, document) = await GetProfilePage(steamId);
+
+            if (document == null || string.IsNullOrEmpty(profileUrl))
+            {
+                await ReplyAsync("Could not find a profile with that Steam ID");
+                return;
+            }
+
+            var profileName = GetProfileName(document);
+
+            var (alien30, alien100, marine30, marine100) = GetKdrStats(document);
+
+            if (string.IsNullOrEmpty(alien30) || string.IsNullOrEmpty(alien100) || string.IsNullOrEmpty(marine30) || string.IsNullOrEmpty(marine100))
+            {
+                await ReplyAsync("Could not find the KDR details for that profile");
+                return;
+            }
+
+            var embed = new EmbedBuilder()
+                .WithTitle(profileName)
+                .WithUrl(profileUrl)
+                .WithDescription("KDR From Last n Rounds")
+                .AddField("Alien KDR | 30:", alien30)
+                .AddField("Alien KDR | 100:", alien100)
+                .AddField("Marine KDR | 30", marine30)
+                .AddField("Marine KDR | 100", marine100)
+                .WithColor(Color.Orange)
+                .Build();
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+        }
+
+        private (string Alien30, string Alien100, string Marine30, string Marine100) GetKdrStats(HtmlDocument profilePage)
+        {
+            var kdrHeader = profilePage.DocumentNode.SelectSingleNode("//h2[contains(text(), 'Ingame')]");
+
+            if (kdrHeader == null)
+                return (null, null, null, null);
+
+            var kdrTable = kdrHeader.NextSibling;
+            while (!kdrTable.Name.Contains("table"))
+            {
+                kdrTable = kdrTable.NextSibling;
+                if (kdrTable == null)
+                    break;
+            }
+
+            if (kdrTable == null)
+                return (null, null, null, null);
+
+            var kdrTableBody = kdrTable.ChildNodes.FindFirst("tbody");
+
+            var kdrAlienHeader = kdrTableBody.ChildNodes.Select(x => x.ChildNodes.FirstOrDefault(z => z.Name == "th" && z.InnerText == "Alien"))
+                .FirstOrDefault(x => x != null);
+            var kdrMarineHeader = kdrTableBody.ChildNodes.Select(x => x.ChildNodes.FirstOrDefault(z => z.Name == "th" && z.InnerText == "Marine"))
+                .FirstOrDefault(x => x != null);
+
+            if (kdrAlienHeader == null || kdrMarineHeader == null)
+                return (null, null, null, null);
+
+            #region alienKdr
+
+            var kdrAlien30Node = kdrAlienHeader.NextSibling;
+            while (!kdrAlien30Node.Name.Contains("td"))
+            {
+                kdrAlien30Node = kdrAlien30Node.NextSibling;
+                if (kdrAlien30Node == null)
+                    break;
+            }
+            if (kdrAlien30Node == null)
+                return (null, null, null, null);
+
+            var alien30Text = kdrAlien30Node.InnerText.Replace("&#39;", ",");
+
+            var kdrAlien100Node = kdrAlien30Node.NextSibling;
+            while (!kdrAlien100Node.Name.Contains("td"))
+            {
+                kdrAlien100Node = kdrAlien100Node.NextSibling;
+                if (kdrAlien100Node == null)
+                    break;
+            }
+            if (kdrAlien100Node == null)
+                return (null, null, null, null);
+
+            var alien100Text = kdrAlien100Node.InnerText.Replace("&#39;", ",");
+
+            #endregion
+
+            #region marineKdr
+
+            var kdrMarine30Node = kdrMarineHeader.NextSibling;
+            while (!kdrMarine30Node.Name.Contains("td"))
+            {
+                kdrMarine30Node = kdrMarine30Node.NextSibling;
+                if (kdrMarine30Node == null)
+                    break;
+            }
+            if (kdrMarine30Node == null)
+                return (null, null, null, null);
+
+            var marine30Text = kdrMarine30Node.InnerText.Replace("&#39;", ",");
+
+            var kdrMarine100Node = kdrMarine30Node.NextSibling;
+            while (!kdrMarine100Node.Name.Contains("td"))
+            {
+                kdrMarine100Node = kdrMarine100Node.NextSibling;
+                if (kdrMarine100Node == null)
+                    break;
+            }
+            if (kdrMarine100Node == null)
+                return (null, null, null, null);
+
+            var marine100Text = kdrMarine100Node.InnerText.Replace("&#39;", ",");
+
+            #endregion
+
+            return (alien30Text, alien100Text, marine30Text, marine100Text);
         }
     }
 }
