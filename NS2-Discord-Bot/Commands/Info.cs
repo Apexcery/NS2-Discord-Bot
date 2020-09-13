9439cc515bba89816f80268e085eafafd4f047fa
Stuart Gleeson <stuart.gleeson98@outlook.com>
2020-09-13 18:14:40 +0100

    Updated help command to put command descriptions on a new line.

--- NS2-Discord-Bot/Commands/Info.cs
+++ NS2-Discord-Bot/Commands/Info.cs
@@ -17,10 +17,10 @@ namespace NS2_Discord_Bot.Commands
 
             var embed = new EmbedBuilder()
                 .WithTitle("Usage")
-                .AddField("Changelog", $"{prefix}changelog | View the most recent changes to this bot.")
-                .AddField("Link Profile", $"{prefix}link <steam ID> (leave blank to view linked profile) | Link your profile to save typing it for every command.")
-                .AddField("Hive Stats", $"{prefix}hive <steam ID> (leave blank to use linked profile) | View the hive stats for the specified user.")
-                .AddField("KDR Stats", $"{prefix}kdr <steam ID> (leave blank to use linked profile) | View the kdr stats for the specified user.")
+                .AddField("Changelog", $"{prefix}changelog\nView the most recent changes to this bot.")
+                .AddField("Link Profile", $"{prefix}link <steam ID> (leave blank to view linked profile)\nLink your profile to save typing it for every command.")
+                .AddField("Hive Stats", $"{prefix}hive <steam ID> (leave blank to use linked profile)\nView the hive stats for the specified user.")
+                .AddField("KDR Stats", $"{prefix}kdr <steam ID> (leave blank to use linked profile)\nView the kdr stats for the specified user.")
                 .WithColor(Color.GetRandomColor())
                 .Build();
 
